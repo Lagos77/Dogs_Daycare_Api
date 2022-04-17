c441eee6f1cab4d9227da9ae23a44b8de8b3861e
Lagos77 <morenhoe_01@hotmail.com>
2022-04-18 01:12:45 +0200

    Updates

---
+++ precache-manifest.c0d359aef6732dfb29d65237eff16341.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cffb6832f0f42d99563d",
+    "url": "/Dogs_Daycare_Api/static/css/main.519665f6.chunk.css"
+  },
+  {
+    "revision": "cffb6832f0f42d99563d",
+    "url": "/Dogs_Daycare_Api/static/js/main.cffb6832.chunk.js"
+  },
+  {
+    "revision": "8c98aab82769540efb62",
+    "url": "/Dogs_Daycare_Api/static/js/1.8c98aab8.chunk.js"
+  },
+  {
+    "revision": "31e084234a1dd00f22b4",
+    "url": "/Dogs_Daycare_Api/static/js/2.31e08423.chunk.js"
+  },
+  {
+    "revision": "59c8a19057a59401ac98",
+    "url": "/Dogs_Daycare_Api/static/js/runtime~main.59c8a190.js"
+  },
+  {
+    "revision": "4ad6884b7f8f48befb662cebc016c2ab",
+    "url": "/Dogs_Daycare_Api/index.html"
+  }
+];
